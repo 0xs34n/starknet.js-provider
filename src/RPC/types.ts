6fca22056568315462787dbc1f1eb3b43f52d2be
0xs34n <fluffy@diamondpaws.xyz>
2022-08-30 21:21:05 -0400

    rpc cleanup

--- src/RPC/types.ts
+++ src/RPC/types.ts
@@ -25,34 +25,19 @@ type felt = number; // TODO
 
 type BlockHash = felt;
 
-interface GetBlockWithTxHashesRes {
-
-}
+interface GetBlockWithTxHashesRes {}
 
 interface BlockWithTxs {
   status: BlockStatus;
   block_header: {
-    block_hash: felt,
-    parent_hash: felt,
-  }
+    block_hash: felt;
+    parent_hash: felt;
+  };
   block_body_with_txs: {
     items: {
-      transaction: ""
-    }
-  }
+      transaction: "";
+    };
+  };
 }
 
-
-
-
-type BlockStatus = "PENDING" | "ACCEPTED_ON_L2" | "ACCEPTED_ON_L1" | "REJECTED"
-
-interface BlockWithTxHash
-
-//   name: "block_id";
-//   description: "The hash of the requested block, or number (height) of the requested block, or a block tag";
-//   required: true;
-//   schema: {
-//     $ref: "#/components/schemas/BLOCK_ID";
-//   };
-// };
+type BlockStatus = "PENDING" | "ACCEPTED_ON_L2" | "ACCEPTED_ON_L1" | "REJECTED";
