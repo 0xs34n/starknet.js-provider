4e880e6eda28b218a545b1f16900c9b4ccd786b1
0xs34n <fluffy@diamondpaws.xyz>
2022-09-02 17:26:17 -0400

    testing

---
+++ src/test/rpc.test.ts
@@ -0,0 +1,16 @@
+import RPCProvider from "../RPC";
+const RPC_URL = "http://localhost:9545";
+
+const rpc = new RPCProvider(RPC_URL);
+
+describe("getBlockWithTxHashes()", () => {
+  test("getBlockWithTxHashes() with block_number", async () => {
+    const block = await rpc.getBlockWithTxHashes({
+      block_id: { block_number: 10 },
+    });
+
+    // TODO for jung: I think the best way to test is if the response status is 200
+    // but since we are getting the json, not sure what's best way
+    expect(block).toHaveProperty("jsonrpc");
+  });
+});
