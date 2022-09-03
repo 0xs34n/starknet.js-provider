87c669ae3a7f32b150d4350a44fc6df2d01e90b2
AspectArchitect <architect@diamondpaws.xyz>
2022-09-02 22:50:17 -0700

    refactor test structure

--- src/test/rpc.test.ts
+++ src/test/rpc.test.ts
@@ -3,12 +3,26 @@ const RPC_URL = "http://localhost:9545";
 
 const rpc = new RPCProvider(RPC_URL);
 
-const TEST_CONTRACT_ADDR = "0x03CA81a47bf5E646b45382b7FF397fc5A2CbCb193341019bbc75f6C0Fa58de64";
-const TEST_BLOCK_HASH = "0x1554f5ffdb2b911524293b626e5d34cc3b92cbea5f3206294645d6d8338351b";
-const TEST_KEY = "0x0";
-const EXPECTED_RESULT = { jsonrpc: '2.0', result: '0x0', id: '0' };
+describe("getBlockWithTxHashes", () => {
+  test("getBlockWithTxHashes with block_number", async () => {
+    const block = await rpc.getBlockWithTxHashes({
+      block_id: { block_number: 10 },
+    });
 
+    // TODO for jung: I think the best way to test is if the response status is 200
+    // but since we are getting the json, not sure what's best way
+    expect(block).toHaveProperty("jsonrpc");
+  });
+});
+
+// Sean: I'd test different cases like below
+// TODO: instead of using real RPCProvider at line 4,
+//       mock the provider so we don't have to set up PathFinder locally
 describe("getStorageAt", function() {
+  const TEST_CONTRACT_ADDR = "0x03CA81a47bf5E646b45382b7FF397fc5A2CbCb193341019bbc75f6C0Fa58de64";
+  const TEST_BLOCK_HASH = "0x1554f5ffdb2b911524293b626e5d34cc3b92cbea5f3206294645d6d8338351b";
+  const TEST_KEY = "0x999";
+  const EXPECTED_RESULT = "0x0";
 
   const validGetStorageAtCases = [
     {
@@ -30,7 +44,9 @@ describe("getStorageAt", function() {
   ];
 
   test.each(validGetStorageAtCases)("should getStorageAt", async function({ contractAddress, key, blockId, expected }) {
-    expect(await rpc.getStorageAt(contractAddress, key, blockId)).toEqual(expected);
+    const response = await rpc.getStorageAt(contractAddress, key, blockId);
+
+    expect(response.result).toEqual(expected);
   })
 
   const invalidGetStorageAtCases = [
@@ -38,55 +54,29 @@ describe("getStorageAt", function() {
       contractAddress: "",
       key: TEST_KEY,
       blockId: {block_id: {block_number: 49}},
-      expectedError: {
-        jsonrpc: '2.0',
-        error: {
-          code: 20,
-          message: "Contract not found",
-        },
-        id: '0'
-      }
+      expectedError: "Contract not found"
     },
     {
       contractAddress: TEST_CONTRACT_ADDR,
       key: TEST_KEY,
       blockId: {block_id: "pending"},
-      expectedError: {
-        jsonrpc: '2.0',
-        error: {
-          code: -32000,
-          message: "Pending data not supported in this configuration",
-        },
-        id: '0'
-      }
+      expectedError: "Pending data not supported in this configuration"
     }, {
       contractAddress: TEST_CONTRACT_ADDR,
       key: "invalid_key",
       blockId: {block_id: {block_number: 49}},
-      expectedError: {
-        jsonrpc: '2.0',
-        error: {
-          code: -32602,
-          message: 'Invalid nibble found: 0x69 at line 1 column 108'
-        },
-        id: '0'
-      }
+      expectedError: "Invalid nibble found"
     }, {
       contractAddress: TEST_CONTRACT_ADDR,
       key: TEST_KEY,
       blockId: {block_id: {block_number: 0}},
-      expectedError: {
-        jsonrpc: '2.0',
-        error: {
-          code: 20,
-          message: 'Contract not found'
-        },
-        id: '0'
-      }
+      expectedError: "Contract not found"
     },
   ];
 
   test.each(invalidGetStorageAtCases)("should not getStorageAt", async function({ contractAddress, key, blockId, expectedError }) {
-    expect(await rpc.getStorageAt(contractAddress, key, blockId)).toEqual(expectedError);
+    const response = await rpc.getStorageAt(contractAddress, key, blockId);
+
+    expect(response.error.message).toContain(expectedError);
   })
 })
