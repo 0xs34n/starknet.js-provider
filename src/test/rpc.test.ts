2523c370d8a32c5a8e7a58a11b2dbbd2b0c419cd
0xs34n <fluffy@diamondpaws.xyz>
2022-09-04 19:43:55 -0400

    chore: rename test variables

--- src/test/rpc.test.ts
+++ src/test/rpc.test.ts
@@ -26,68 +26,68 @@ describe("JSON RPC Provider", () => {
 
     describe("getBlockWithTxHashes()", () => {
       test.each(blockIds)("blockId: %p", async (blockId) => {
-        const block = await rpc.getBlockWithTxHashes({ block_id: blockId });
-        expect(block).toHaveProperty("result");
+        const response = await rpc.getBlockWithTxHashes({ block_id: blockId });
+        expect(response).toHaveProperty("result");
       });
     });
 
     describe("getBlockWithTxs()", () => {
       test.each(blockIds)("blockId: %p", async (blockId) => {
-        const block = await rpc.getBlockWithTxs({ block_id: blockId });
-        expect(block).toHaveProperty("result");
+        const response = await rpc.getBlockWithTxs({ block_id: blockId });
+        expect(response).toHaveProperty("result");
       });
     });
 
     describe("getStatusUpdate()", () => {
       test.each(blockIds)("blockId: %p", async (blockId) => {
-        const block = await rpc.getStateUpdate({ block_id: blockId });
-        expect(block).toHaveProperty("result");
+        const response = await rpc.getStateUpdate({ block_id: blockId });
+        expect(response).toHaveProperty("result");
       });
     });
 
     describe("getStorageAt()", () => {
       test("block_id default pending", async () => {
-        const block = await rpc.getStorageAt({
+        const response = await rpc.getStorageAt({
           contract_address: contract_address,
           key,
         });
-        expect(block).toHaveProperty("result");
+        expect(response).toHaveProperty("result");
       });
 
       test.each(blockIds)("blockId: %p", async (blockId) => {
-        const block = await rpc.getStorageAt({
+        const response = await rpc.getStorageAt({
           contract_address: contract_address,
           key,
           block_id: blockId,
         });
-        expect(block).toHaveProperty("result");
+        expect(response).toHaveProperty("result");
       });
     });
 
     describe("getTransactionByHash()", () => {
       test(`transaction_hash = ${transaction_hash}`, async () => {
-        const block = await rpc.getTransactionByHash({
+        const response = await rpc.getTransactionByHash({
           transaction_hash,
         });
-        expect(block).toHaveProperty("result");
+        expect(response).toHaveProperty("result");
       });
     });
 
     // not be implemented in pathfinder - skip
     describe.skip("traceTransaction()", () => {
       test(`tx hash = ${transaction_hash}`, async () => {
-        const block = await rpc.traceTransaction({
+        const response = await rpc.traceTransaction({
           transaction_hash: transaction_hash,
         });
-        expect(block).toHaveProperty("result");
+        expect(response).toHaveProperty("result");
       });
     });
 
     // not implemented in pathfinder - skip
     describe.skip("traceBlockTransactions()", () => {
       test(``, async () => {
-        const block = await rpc.traceBlockTransactions("");
-        expect(block).toHaveProperty("result");
+        const response = await rpc.traceBlockTransactions("");
+        expect(response).toHaveProperty("result");
       });
     });
   });
